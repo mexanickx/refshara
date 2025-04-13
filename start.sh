a5a76d9ef9ff39f4f4abf72b60d068a4abf1f64b
mexanickx <broynikirill@mail.ru>
2025-04-13 17:34:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-# Устанавливаем переменные окружения для Flask
-export FLASK_APP=ref.py
-export FLASK_RUN_HOST=0.0.0.0
-export FLASK_RUN_PORT=${PORT:-5000}
-
-# Запуск приложения с помощью Gunicorn
-gunicorn -w 4 -b 0.0.0.0:$FLASK_RUN_PORT ref:app
+python ref.py
