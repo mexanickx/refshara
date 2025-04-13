3f6e2dbe105eb7c7a5a16e98f9fe00dad71b5d78
mexanick <broynikirill@mail.com>
2025-04-13 16:58:35 +0300

    Ваше сообщение о коммите

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Запуск Python-скрипта ref.py
-python ref.py
+# Устанавливаем переменные окружения для Flask
+export FLASK_APP=ref.py
+export FLASK_RUN_HOST=0.0.0.0
+export FLASK_RUN_PORT=${PORT:-5000}
 
-# Запуск приложения и привязка к порту 8080
-./myapp --port=18012
+# Запуск приложения с помощью Gunicorn
+gunicorn -w 4 -b 0.0.0.0:$FLASK_RUN_PORT ref:app
